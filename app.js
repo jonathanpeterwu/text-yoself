10486f850d198952485004b5bfae5ad22b87eeed
jonathanpeterwu <jpwu03@yahoo.com>
2014-10-16 19:14:36 -0700

    initial framework

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// New Code
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var expenses = require('./routes/expenses');
 
 var app = express();
 
@@ -14,25 +19,31 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
-// catch 404 and forward to error handler
+app.use('/', routes);
+app.use('/expenses', expenses);
+
+
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
+/// error handlers
 
 // development error handler
 // will print stacktrace
@@ -55,6 +66,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var Keen = require('keen.io');
+
+var keen = Keen.configure({
+    projectId: "<project_id>",
+    writeKey: "<write_key>",
+    readKey: "<read_key>",
+    masterKey: "<master_key>"
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
+	// send back response
   res.render('index', { title: 'Express' });
 });
 
+router.get('/expenses', function(req, res) {
+
+	var count = new Keen.Query("count", {
+  event_collection: "expenses",
+  group_by: "expense",
+  timeframe: "this_7_days"
+	});
+
+	var total = count || 300;
+  res.render('expenses', { total: total });
+	// send back esponse for last 7 days using keen
+});
+
+router.post('/expenses/new', function(req, res) {
+	var expense = req.body.total
+
+	// send single event to Keen IO
+	keen.addEvent("expenses", {"expense": expense}, function(err, res) {
+	    if (err) {
+	        console.log("Oh no, an error!");
+	    } else {
+	        console.log("Hooray, it worked!");
+	    }
+	});
+
+});
 module.exports = router;
